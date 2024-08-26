31812d9e13715d708e3a7b26e4057368ef0ade9f
lijis1 <s.lijis.liji@gmail.com>
2024-08-26 12:34:00 +0530

    first commit

---
+++ cubesum.py
@@ -0,0 +1,5 @@
+sum=0
+x=int(input("enter a limit"))
+for i in range(1,x+1):
+    sum=sum+pow(i,3)
+print(sum)
---
+++ pairchar.py
@@ -0,0 +1,8 @@
+x = 'hello'
+xset=set(x)
+y = 'world'
+yset=set(y)
+ans_set = xset.intersection(yset)
+print(ans_set)
+print(len(ans_set))
+
